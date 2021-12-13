0ab2ae07432dd244dcbc331f10df84a9c2242d8f
Kristers Bitainis <bitmens@gmail.com>
2021-12-14 01:48:15 +0200

    .

--- app/src/main/java/com/fifth_group/repositoryg/FirstFragment.java
+++ app/src/main/java/com/fifth_group/repositoryg/FirstFragment.java
@@ -1,15 +1,22 @@
 package com.fifth_group.repositoryg;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.fifth_group.repositoryg.databinding.FragmentFirstBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class FirstFragment extends Fragment {
 
@@ -28,6 +35,36 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        binding.saveInput.setOnClickListener(v -> {
+            editor.putString("text", binding.inputField.getText().toString());
+            editor.apply();
+            Toast.makeText(getActivity(), "Saved", Toast.LENGTH_LONG).show();
+
+        });
+
+        binding.spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG).show();
+                if(position == 0) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                if(position == 1) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/fifth_group/repositoryg/SecondFragment.java
+++ app/src/main/java/com/fifth_group/repositoryg/SecondFragment.java
@@ -1,12 +1,17 @@
 package com.fifth_group.repositoryg;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.fifth_group.repositoryg.databinding.FragmentSecondBinding;
@@ -33,7 +38,23 @@ public class SecondFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                        .navigate(R.id.FirstFragment);
+
+
+            }
+
+        });
+
+        binding.btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                String check = sharedPref.getString("text", "");
+
+                binding.textviewSecond.setText(check);
+                if (check == "") {
+                    Toast.makeText(getActivity(), "Nothing Found", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
